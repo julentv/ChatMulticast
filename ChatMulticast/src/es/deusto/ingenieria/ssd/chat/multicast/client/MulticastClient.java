b9e569169beab2cb41ddd20261909bdeaf734631
RaquelCagigas <raquel.cagigas@opendeusto.es>
2013-10-22 19:38:52 +0200

    jdd

--- ChatMulticast/src/es/deusto/ingenieria/ssd/chat/multicast/client/MulticastClient.java
+++ ChatMulticast/src/es/deusto/ingenieria/ssd/chat/multicast/client/MulticastClient.java
@@ -1,7 +1,17 @@
 package es.deusto.ingenieria.ssd.chat.multicast.client;
 
+import es.deusto.ingenieria.ssd.chat.multicast.controller.Controller;
+
+
+
 public class MulticastClient extends Thread {
 
+private Controller controller;
+	
+	public MulticastClient(Controller controller){
+		this.controller=controller;
+	}
+	
 	
 
 }
